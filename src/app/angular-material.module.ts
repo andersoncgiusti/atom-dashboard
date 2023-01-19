7e6ba2165ec37c57ff683d62bab9e3497edad8c9
Anderson-Giusti <anderson.giusti@ibm.com>
2023-01-19 18:37:26 -0300

    development menu

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from "@angular/core";
-import { } from "@angular/material";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  exports: []
+  exports: [
+    MatTabsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule
+  ]
 })
 export class AngularMaterialModule {}
