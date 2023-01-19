7711d10380d29a0ddfe3d0b405da5827ec5b6574
Anderson-Giusti <anderson.giusti@ibm.com>
2023-01-19 11:23:07 -0300

    install angular-material and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from "./angular-material.module";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
