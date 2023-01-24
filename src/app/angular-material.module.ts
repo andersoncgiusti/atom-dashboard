a1b7f7053906c78119f3f05b9ebd9bd6855cf5c4
Anderson-Giusti <anderson.giusti@ibm.com>
2023-01-24 12:32:29 -0300

    updated menu

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -8,6 +8,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   exports: [
@@ -19,7 +20,8 @@ import { MatMenuModule } from '@angular/material/menu';
     MatDividerModule,
     MatToolbarModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTooltipModule
   ]
 })
 export class AngularMaterialModule {}
